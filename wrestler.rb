2379bfb692d19d74a4031b21c436108673567c7d
alanna-wing <alanna.wing@gmail.com>
2016-11-25 10:46:07 +0000

    Added header, asks questions and stores variables

---
+++ wrestler.rb
@@ -0,0 +1,10 @@
+puts "What is your wrestler name?"
+sleep(1)
+
+print "The name of your first pet: "
+first_pet = gets.chomp
+sleep(0.5)
+
+print "Name of the first street you lived on: "
+first_street = gets.chomp
+sleep(0.5)
