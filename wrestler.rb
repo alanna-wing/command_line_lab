6d53a204ab2dc42260d9bbb501a604f947be4cc1
alanna-wing <alanna.wing@gmail.com>
2016-11-25 11:10:53 +0000

    added name output and updated questions

--- wrestler.rb
+++ wrestler.rb
@@ -1,10 +1,13 @@
 puts "What is your wrestler name?"
 sleep(1)
 
-print "The name of your first pet: "
+print "Name of your first pet: "
 first_pet = gets.chomp
 sleep(0.5)
 
-print "Name of the first street you lived on: "
+print "First street you lived on: "
 first_street = gets.chomp
 sleep(0.5)
+
+name = [first_pet, first_street].join(' ');
+puts "Your wrestler name is: #{name}!"
